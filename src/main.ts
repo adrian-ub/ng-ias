1607c55cc20f6528635ee1be4da5b4267ae53b50
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-06-30 11:30:38 -0500

    feat: add mocks configuration

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err)
+import { worker } from '../mocks';
+
+worker.then(() =>
+  bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error(err)
+  )
 );
