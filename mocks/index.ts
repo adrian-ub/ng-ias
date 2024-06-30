83acdce36eda03b6ac0626bf110b07ac0c5e14b3
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-06-30 12:03:55 -0500

    fix: service worker configuration for mock

--- mocks/index.ts
+++ mocks/index.ts
@@ -1,6 +1,10 @@
+import { isDevMode } from '@angular/core';
 import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 
 export const worker = setupWorker(...handlers).start({
   onUnhandledRequest: 'bypass',
+  serviceWorker: {
+    url: isDevMode() ? '/mockServiceWorker.js' : '/ng-ias/mockServiceWorker.js',
+  },
 });
