0bdb4bbcae38ebd6e2ad259fc4ac35a6fdaf628d
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-06-30 13:26:24 -0500

    chore: update dependency for icon components

--- src/components/accordion/lib/hlm-accordion-icon.directive.ts
+++ src/components/accordion/lib/hlm-accordion-icon.directive.ts
@@ -1,27 +1,30 @@
 import { Directive, computed, inject, input } from '@angular/core';
 import { lucideChevronDown } from '@ng-icons/lucide';
 import { hlm } from '@spartan-ng/ui-core';
-import { HlmIconComponent, provideIcons } from '@spartan-ng/ui-icon-helm';
+import { HlmIconComponent, provideIcons } from '@components/icon';
 import type { ClassValue } from 'clsx';
 
 @Directive({
-	selector: 'hlm-icon[hlmAccordionIcon], hlm-icon[hlmAccIcon]',
-	standalone: true,
-	providers: [provideIcons({ lucideChevronDown })],
-	host: {
-		'[class]': '_computedClass()',
-	},
+  selector: 'hlm-icon[hlmAccordionIcon], hlm-icon[hlmAccIcon]',
+  standalone: true,
+  providers: [provideIcons({ lucideChevronDown })],
+  host: {
+    '[class]': '_computedClass()',
+  },
 })
 export class HlmAccordionIconDirective {
-	private readonly _hlmIcon = inject(HlmIconComponent);
+  private readonly _hlmIcon = inject(HlmIconComponent);
 
-	public readonly userClass = input<ClassValue>('', { alias: 'class' });
-	protected _computedClass = computed(() =>
-		hlm('inline-block h-4 w-4 transition-transform [animation-duration:200]', this.userClass()),
-	);
+  public readonly userClass = input<ClassValue>('', { alias: 'class' });
+  protected _computedClass = computed(() =>
+    hlm(
+      'inline-block h-4 w-4 transition-transform [animation-duration:200]',
+      this.userClass()
+    )
+  );
 
-	constructor() {
-		this._hlmIcon.size = 'none';
-		this._hlmIcon.name = 'lucideChevronDown';
-	}
+  constructor() {
+    this._hlmIcon.size = 'none';
+    this._hlmIcon.name = 'lucideChevronDown';
+  }
 }
