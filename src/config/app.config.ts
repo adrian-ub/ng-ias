c94caf34eef11b939e6d6d75a8051b6f9aa5e3fc
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2024-07-01 09:28:58 -0500

    feat: Update app.config.ts to provide APP_BASE_HREF for '/ng-ias/' and update providers

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,7 +5,13 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRecipe } from '@infraestructure/driven-adapter/recipe';
 
 import { appRoutes } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideHttpClient(), provideRecipe()],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/ng-ias/' },
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    provideRecipe(),
+  ],
 };
